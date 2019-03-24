a5cbc98540daf564192a60f1543e99ef14564aa2
YAGHMAYI <yaghmayi@gmail.com>
2019-03-24 16:25:48 +0100

    First Commit

---
+++ Main/Foo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutureSkill.Main
+{
+    public class Foo
+    {
+        public int M1()
+        {
+            return 1;
+        }
+    }
+}
---
+++ UnitTests/FooTest.cs
@@ -0,0 +1,17 @@
+﻿using FutureSkill.Main;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class FooTest
+    {
+        [TestMethod]
+        public void M1Test()
+        {
+            Foo f = new Foo();
+            Assert.AreEqual(1, f.M1());
+        }
+
+    }
+}
